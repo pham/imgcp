05582a1ad432858093130436ce79759072f7a26c
paul pham <paul@aquaron.com>
2017-08-08 09:54:50 -0700

    Version 1.4. Fix out of storage problems. Check for variations in exisiting dir names. More info on system

--- imgcp.pl
+++ imgcp.pl
@@ -7,18 +7,21 @@ use Ptk::BasicWindow;
 use ReadDir;
 use File::Path;
 use File::Copy;
+use Win32::DriveInfo;
 
-use constant PRODUCT    => 'imgcp v1.3';
+use constant PRODUCT    => 'imgcp v1.4';
 use constant FG         => '#EAEAEA';
 use constant BG         => '#4F6D7A';
 use constant HBG        => '#869CA5';
+use constant SPACEREQ   => 1024 * 1024 * 1024; #243419574272;
+use constant AUTOEX     => q{ind,inp,bin,bdm,cpi,dat,mpl,thm,pod,xml,bnp,int,txt,html,ctg};
 use constant HELP       => <<EOT;
 imgcp -source D: -target C:\/tmp -auto -ex tmp,ext
 
--source > Dir of where files are
--target > Where to copy files to
--auto   > Start copying immediately
--ex     > List of extensions to exclude
+-source \x{2022} Dir of where files are
+-target \x{2022} Where to copy files to
+-auto   \x{2022} Start copying immediately
+-ex     \x{2022} List of extensions to exclude
 EOT
 use vars qw /$STATUS $BW $CL/;
 select((select(STDOUT), $|=1)[0]);
@@ -46,12 +49,23 @@ sub main {
     my $middle  = $top->Frame(-bg,BG)->pack(qw/-side left -fill y -anchor w/);
     my $bottom  = $mw->Frame(-bg,BG)->pack(qw/-side bottom/);
 
-    $CL->{-source} ||= '';
-    $CL->{-target} ||= '';
+    $CL->{-source}            ||= '';
+    $CL->{-target}            ||= '';
+    $CL->{-tinfo}               = _drive_type($CL->{-target});
+    $CL->{-sinfo}               = _drive_type($CL->{-source});
+    my $datasize                = $CL->{-sinfo}->{total} - $CL->{-sinfo}->{free};
+    my $sizereq                 = $CL->{-tinfo}->{free} ? ($datasize / $CL->{-tinfo}->{free}) * 100 : 0;
 
-    if (-d $CL->{-source} && -d $CL->{-target}) {
-        $mw->{buttonLabel}  = sprintf qq{Copy from %s}, $CL->{-source};
-        $mw->{button}       = $BW->button($middle, {
+    $mw->bind('<Escape>' => sub { exit(0); });
+
+	if (!$CL->{-source} || !$CL->{-target}) {
+        $STATUS     = HELP;
+	} elsif ($CL->{-tinfo}->{type} eq q{unknown} or $CL->{-tinfo}->{free} < SPACEREQ or $sizereq > 90) {
+        $STATUS     = sprintf q{Destination (%s) is invalid or does not have enough storage space (%s)!},
+                        $CL->{-target}, _space($CL->{-tinfo}->{free});
+    } elsif (-d $CL->{-source} && -d $CL->{-target}) {
+        $mw->{buttonLabel}      = q{Start copy};
+        $mw->{button}           = $BW->button($middle, {
             text    => \$mw->{buttonLabel},
             cb      => \&_cat,
             font    => q{Verdana 10},
@@ -59,9 +73,20 @@ sub main {
         })->pack(qw/-side top -anchor w/);
 
         $BW->progress($bottom);
+        $STATUS     = sprintf qq{From %s %s %s (%s data)\nTo %s %s %s (%s free)\nStorage required: %d%%},
+                        $CL->{-source}, 
+                        _space($CL->{-sinfo}->{total}),
+                        $CL->{-sinfo}->{type},
+                        _space($datasize),
+                        $CL->{-target}, 
+                        _space($CL->{-tinfo}->{total}),
+                        $CL->{-tinfo}->{type},
+                        _space($CL->{-tinfo}->{free}),
+                        $sizereq;
 
+        $mw->bind('<Escape>' => undef);
         $mw->bind('<Return>' => \&_cat);
-		$CL->{-auto} and $CL->{-runit} = 1;
+        $CL->{-auto} and $CL->{-runit} = 1;
     } elsif (-d $CL->{-target} and !-d $CL->{-source}) {
         $STATUS     = qq{Can't find files at "-source" dir [$CL->{-source}]};
     } elsif (!-d $CL->{-target} and -d $CL->{-source}) {
@@ -69,6 +94,9 @@ sub main {
     } else {
         $STATUS     = HELP;
     }
+
+    _get_target_dirs($CL->{-target});
+
     $middle->Label(
         -textvariable   => \$STATUS,
         -bg             => BG,
@@ -88,50 +116,92 @@ sub _cat {
         my $f               = shift;
         my $ctime           = (stat "$src/$f")[9];
         my ($dd,$mm,$yy)    = (localtime ($ctime))[3,4,5];
-        my $dest            = sprintf "%s/%04d%02d%02d", 
-            $CL->{-target}, $yy+1900, $mm+1, $dd;
+        my $yymmdd          = sprintf "%04d%02d%02d", $yy+1900, $mm+1, $dd;
+        my $dest            = sprintf "%s/%s", $CL->{-target}, $yymmdd;
+        my $destprev        = $CL->{-datedir}->{$yymmdd};
+
+        ### check if a version of destination already exists
+        foreach (@$destprev) {
+            my $filepath    = $CL->{-target}.'/'.$_.'/'.$f;
+            return 0        if -s $filepath;
+        }
 
         mkpath ($dest,0,0755) unless -d $dest;
 
         ### make sure we're not overwriting, it's inefficient
         unless (-s "$dest/$f") {
-            copy ("$src/$f", "$dest/$f");
+            unless (copy ("$src/$f", "$dest/$f")) {
+                return -1;
+            }
             return 1;
         }
         return 0;
     };
     my %hit     = ();
     my $mw      = $BW->{-mw};
+    $CL->{-ex}  = AUTOEX . ',' . ($CL->{-ex}||'');
 
-    foreach my $e (split /,/, ($CL->{-ex}||'')) {
+    foreach my $e (split /,/, $CL->{-ex}) {
         $hit{uc $e} = 0;
     }
 
     $mw->{button}->configure(-state => 'disabled');
-    $mw->bind('<Return>', '');
+    $mw->bind('<Return>' => '');
 
     &_read_dirs(
         $CL->{-source},
         $funcf,
         \$c,
-        $t,
+        \$t,
         \%hit
     );
 
-    $mw->{buttonLabel} = 'Done';
     $mw->bind('<Escape>', sub { exit(0); } );
 
-    $STATUS = $c . "/" . $t . " Files copied\n";
-
+    my %goodbad     = ();
     foreach (sort { $hit{$b} <=> $hit{$a} } keys %hit) {
         $hit{$_} or next;
-        $STATUS .= $hit{$_} > 0 
-            ? (sprintf "%d/%s ", $hit{$_}, $_)
-            : (sprintf "%s ", lc $_);
+        my $key         = $hit{$_} > 0 ? 'good' : 'bad';
+        $goodbad{$key}{label}   .= sprintf "%d\x{2219}%s ", abs($hit{$_}), $_;
+        $goodbad{$key}{count}   += abs($hit{$_});
     }
+    my $info        = sprintf qq{Targets %d\x{2219}\x{2219}\x{203a} %s\nIgnored %d\x{2219}\x{2219}\x{203a} %s},
+                        $goodbad{good}{count},
+                        $goodbad{good}{label},
+                        $goodbad{bad}{count},
+                        lc $goodbad{bad}{label};
     
-    $BW->{-progress} = 100;
+    if ($c > 0 && $c == $goodbad{good}{count}) {
+        $STATUS = qq{FINISHED: $c file(s) copied\n$info};
+        $BW->{-progress} = 100;
+    } elsif ($c > 0) {
+        $STATUS = sprintf qq{ERROR: %d/%d file(s) copied\n%s}, 
+            $c, $goodbad{good}{count}, $info;
+    } elsif ($c < 0) {
+        $STATUS = sprintf qq{ERROR: No space left on device (needs > %s)\n%s}, 
+            _space(SPACEREQ), $info;
+    } else {
+        $STATUS = sprintf qq{ERROR: Nothing copied out of %d file(s)!\n%s},
+            $goodbad{good}{count}, $info;
+    }
+    $mw->{button}->packForget;
+
     $mw->update;
+}
+
+sub _get_target_dirs {
+    my $dir         = shift;
+    $CL->{-datedir} = ();
+
+    if (opendir my $dh, $dir) {
+        my @objs    = grep /^\d{8}/, readdir($dh);
+        closedir $dh;
+
+        foreach my $obj (@objs) {
+            next unless $obj =~ /^(\d{8}).*$/;
+            push @{ $CL->{-datedir}{$1} }, $&;
+        }
+    }
 }
 
 sub _read_dirs {
@@ -150,17 +220,80 @@ sub _read_dirs {
                 }
                 $ext->{$e}++;
             }
+
             if (-d "$dir/$obj") {
                 &_read_dirs("$dir/$obj", $funcf, $c, $t, $ext);
+            } elsif ($obj =~ /\.(.*)$/) {
+                ### copy only files with extensions
+                ### check disk space before copy
+                if (_has_space("$dir/$obj")) {
+                    $STATUS             = "$dir\n$obj";
+                    my $res             = &$funcf($dir,$obj);
+                    if ($res < 0) {
+                        $$c             = -1;
+                        last;
+                    }
+                    $$c                += $res;
+                    $BW->{-progress}    = $$c/$$t*100;
+                    $BW->{-mw}->{buttonLabel} = $$c . '/' . $$t;
+                    $BW->{-mw}->update;
+                } else {
+                    $$c                 = -1;
+                    last;
+                }
             } else {
-                $STATUS             = "$dir\n$obj";
-                $$c                += &$funcf($dir,$obj);
-                $BW->{-progress}    = $$c/$t*100;
-                $BW->{-mw}->{buttonLabel} = $$c . '/' . $t;
-                $BW->{-mw}->update;
+                ### files without extensions, don't count
+                $$t--;
             }
         }
     }
+}
+
+sub _has_space {
+    my $file    = shift;
+    my $drive   = $CL->{-target};
+    if ($drive  =~ /^([^:]+):/) {
+        $drive  = $1;
+    }
+    my $free    = (Win32::DriveInfo::DriveSpace($drive))[6];
+
+    if ((stat $file)[7]+SPACEREQ > $free) {
+        return 0;
+    }
+    return 1;
+}
+
+sub _space {
+    my $bytes           = shift || 0;
+    if ($bytes >= (1024 * 1024 * 1024)) {
+        return sprintf qq{%d GB}, $bytes / (1024 * 1024 * 1024);
+    } elsif ($bytes >= (1024 * 1024)) {
+        return sprintf qq{%d MB}, $bytes / (1024 * 1024);
+    }
+    return sprintf qq{%d KB}, $bytes / 1024;
+}
+
+sub _drive_type {
+    my $drive   = $_[0];
+    if ($drive  =~ /^([^:]+):/) {
+        $drive  = $1;
+    }
+    my $type                = Win32::DriveInfo::DriveType($drive);
+    my ($total,$free)       = (Win32::DriveInfo::DriveSpace($drive))[5,6];
+
+    if      (!$type)     { $type = q{unknown};   }
+    elsif   ($type == 2) { $type = q{removable}; } 
+    elsif   ($type == 3) { $type = q{fixed};     } 
+    elsif   ($type == 4) { $type = q{network};   } 
+    elsif   ($type == 5) { $type = q{cd-rom};    } 
+    elsif   ($type == 6) { $type = q{ram};       }
+    else                 { $type = q{unknown};   }
+
+    return {
+        type    => $type,
+        total   => $total || 0,
+        free    => $free  || 0
+    };
 }
 
 1;
